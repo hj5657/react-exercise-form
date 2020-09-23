945624a538975d53a1cc4a7a78ca3dc777a46693
hejie <565703319@qq.com>
2020-09-23 18:11:13 +0800

    [hejie]: form

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,14 +1,103 @@
-import React, {Component} from 'react';
-import './myProfile.less';
+import React, { Component } from "react";
+import "./myProfile.less";
 
 class MyProfile extends Component {
+  state = {
+    name: "",
+    gender: "",
+    description: "",
+    read: "",
+  };
+  handlChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  handlSumit = (event) => {
+    event.preventDefault();
+    console.log(JSON.stringify(this.state));
+  };
+  handlClick = (event) => {
+    if (event.target.value === "") {
+      this.setState({
+        read: "checked",
+      });
+    } else {
+      this.setState({
+        read: "",
+      });
+    }
+  };
   render() {
     return (
-      <form></form>
+      <div className="content">
+        <h1>My Profile</h1>
+        <form onSubmit={this.handlSumit}>
+          <label>
+            <span>Name</span>
+            <br />
+            <input
+              type="text"
+              placeholder="Your name"
+              name="name"
+              onChange={this.handlChange}
+              value={this.state.name}
+            />
+          </label>
+          <label>
+            <span>Gender</span>
+            <br />
+            <select
+              name="gender"
+              onChange={this.handlChange}
+              value={this.state.gender}
+            >
+              <option name="gender" value="" />
+              <option name="gender" value="male">
+                Male
+              </option>
+              <option name="gender" value="female">
+                Female
+              </option>
+            </select>
+          </label>
+          <label>
+            <span>Description</span>
+            <br />
+            <textarea
+              name="description"
+              placeholder="Description about yourself"
+              onChange={this.handlChange}
+              value={this.state.description}
+            />
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="read"
+              onClick={this.handlClick}
+              value={this.state.read}
+              className="checkbox"
+            />
+            I have read the terms of conduct
+          </label>
+
+          <input
+            type="submit"
+            value="Submit"
+            name="submit"
+            className="submit"
+            disabled={
+              !this.state.name ||
+              !this.state.gender ||
+              !this.state.description ||
+              !this.state.read
+            }
+          />
+        </form>
+      </div>
     );
   }
 }
 
 export default MyProfile;
-
-
